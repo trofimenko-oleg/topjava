cd3ca875ee07c83a4566feabd41d1b85bfac4b47
bethrezene <oleg.trophymenko@i.ua>
2020-04-18 19:17:55 +0300

    11 10 restrict modification

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -10,7 +10,8 @@ public class Profiles {
 
     public static final String
             POSTGRES_DB = "postgres",
-            HSQL_DB = "hsqldb";
+            HSQL_DB = "hsqldb",
+            HEROKU = "heroku";
 
     public static final String ACTIVE_DB = POSTGRES_DB;
 
